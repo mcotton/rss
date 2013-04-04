0a19d698e0eff6ed539b5f82352f3cb85a58b9c3
Mark Cotton <mcotton@mcottondesign.com>
2013-04-03 23:00:35 -0500

    importing feeds, fetching all entries for those feeds and saving to the couch

---
+++ README.md
@@ -0,0 +1,13 @@
+rss
+===
+
+joining in on the fun and making my own RSS reader
+
+### installation ###
+
+ - install node.js v0.10.x
+ - run npm install
+ - install couchdb and create a database named feeds
+ - export your feed from Google Reader and save it as subscription.xml
+
+
---
+++ app.js
@@ -0,0 +1,82 @@
+var OpmlParser = require('opmlparser')
+      , parser = new OpmlParser()
+      , RSSparser = require('blindparser')
+      , fs = require('fs')
+      , request = require('request')
+      , repl = require('repl')
+      , couch = require('nano')('http://localhost:5984')
+      ;
+
+var feed_store = couch.use('feeds');
+
+
+function myCallback (error, meta, feeds, outline){
+    if (error) {
+        console.error(error) 
+    } else {
+        console.log('OPML info');
+        console.log('%s - %s - %s', meta.title, meta.dateCreated, meta.ownerName);
+        console.log('Feeds');
+        feeds.forEach(function (feed){
+            console.log('%s - %s (%s)', feed.title, feed.htmlurl, feed.xmlurl);
+            feed_store.insert({ 
+                                title:      feed.title,
+                                htmlUrl:    feed.htmlurl,
+                                xmlUrl:     feed.xmlurl,
+                                type:       feed.type,
+                                text:       feed.text
+                               }, feed.title, function(err, body) {
+                if (!err)
+                    console.log(body);
+                });
+        });
+    }
+}
+
+function parseFile() {
+    parser.parseFile('./subscriptions.xml', myCallback);
+}
+
+function listFeeds() {
+    feed_store.list(function(err, body) {
+        if (!err) {
+            body.rows.forEach(function(doc) {
+                console.log(doc.id);
+                fetchFeed(doc.id);
+            });
+        }
+    });
+}
+
+function fetchFeed(docID) {
+    feed_store.get(docID, function(err, body) {
+        console.log('Fetching feed for ' + docID);
+        console.log('Title: ' + body.title);
+        console.log('URL: ' + body.xmlUrl);
+        RSSparser.parseURL(body.xmlUrl, function(err, out){
+            if(err) console.log(err)
+            if(out) updateFeed(docID, body, out);
+        });
+    });
+}
+
+function updateFeed(docID, doc, parsedFeed) {
+    doc.items = parsedFeed.items;
+    feed_store.insert(doc, docID, function(err, body) {
+        if(err) console.log('error updated feed');
+        if(body) console.log('updated feed for ' + doc.title);
+    }); 
+}
+
+listFeeds();
+//parseFile();
+//fetchFeed('mcotton-shooting');
+
+/*
+repl.start({
+    prompt: "node via stdin> ",
+    input: process.stdin,
+    output: process.stdout
+});
+*/
+
